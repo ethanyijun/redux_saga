79026245f31abcb869ab01861e4936cabe282f0e
Yijun Gai <ethan.yijun@gmail.com>
2019-02-23 18:58:31 +1100

    Repo for redux saga

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { updateUser,apiRequest } from './actions/user-actions';
+import { createSelector } from 'reselect';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.onUpdateUser = this.onUpdateUser.bind(this);
+  }
+
+
+  onUpdateUser(event){
+    this.props.onUpdateUser(event.target.value);
+  }
+  render() {
+  
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <input onChange={this.onUpdateUser}/>
+          {this.props.user}
+        </header>
+      </div>
+    );
+  }
+}
+
+const productsSelector = createSelector(
+  state => state.products,
+  products => products
+);
+
+const userSelector = createSelector(
+  state => state.user,
+  user => user
+);
+
+const mapStateToProps = createSelector(
+  productsSelector,
+  userSelector,
+  (products, user) => ({
+    products,
+    user
+  })
+);
+
+ const mapActionsToProps =  {
+    onUpdateUser: updateUser,
+    onApiRequest: apiRequest
+ }
+
+
+
+export default connect(mapStateToProps,mapActionsToProps)(App);
